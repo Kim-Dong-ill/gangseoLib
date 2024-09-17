b1995545dd01a88f8c5b786a5399a06109fea74d
Kim-Dong-ill <150985297+Kim-Dong-ill@users.noreply.github.com>
2024-09-17 22:25:48 +0900

    모달

--- src/components/MenusScrollFrame.js
+++ src/components/MenusScrollFrame.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/css/sass/menusScrollFrame.scss";
-
+import "../assets/css/sass/modal.scss";
+import Modal from "./Layout/Modal";
 function MenusScrollFrame() {
+  const [modalState, setModalState] = useState(false);
+
+  const modalOpen = () => {
+    setModalState(true);
+  };
+  const modalClose = () => {
+    setModalState(false);
+  };
+
   return (
     <div className="menusScroll frame background">
+      <Modal isOpen={modalState} onClose={modalClose} />
       <div className="scrollFrame">
         <div className="frameInner">
-          <div className="InnerContent"></div>
+          <div className="InnerContent">
+            <button
+              onClick={() => {
+                modalOpen();
+              }}
+            >
+              a
+            </button>
+          </div>
         </div>
       </div>
     </div>
