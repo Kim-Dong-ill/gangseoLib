bc59db894ba727de5987ae20f032e503d0a7b880
Kim-Dong-ill <150985297+Kim-Dong-ill@users.noreply.github.com>
2024-09-18 01:14:25 +0900

    테이블 중간

--- src/pages/Libabout/menus/Printing.js
+++ src/pages/Libabout/menus/Printing.js
@@ -1,8 +1,16 @@
 import React from "react";
 import MenusScrollFrame from "../../../components/MenusScrollFrame";
+import { SubBackWrap } from "../../../components/Layout/BackWrap";
+import { SubTopMenu } from "../../../components/Layout/TopMenu";
+import PrintingData from "../datas/Printing/PrintingData";
 
 function Printing() {
-  return <div><MenusScrollFrame /></div>;
+  return (
+    <SubBackWrap>
+      <SubTopMenu />
+      <MenusScrollFrame children={<PrintingData />} />
+    </SubBackWrap>
+  );
 }
 
 export default Printing;
