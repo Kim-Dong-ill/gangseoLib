a5e37139241d263a7f0c5fe47da505256b0a554d
blueheeya83 <blueheeya12@gmail.com>
2024-09-19 02:22:04 +0900

    시설 안내 페이지 레이아웃 완료 자율학습실, 노트북실 상세 완료

--- src/pages/Libabout/datas/FacilityUse/FacilityUseData.js
+++ src/pages/Libabout/datas/FacilityUse/FacilityUseData.js
@@ -1,7 +1,4 @@
 import React from "react";
-import restaurantImg01 from "../../../../assets/images/restaurantImg01.png";
-import restaurantImg02 from "../../../../assets/images/restaurantImg02.png";
-import restaurantImg03 from "../../../../assets/images/restaurantImg03.png";
 import FirestTitle from "../../../../components/TextBoxs/CntTitle";
 import CntWrap from "../../../../components/TextBoxs/CntWrap";
 
@@ -14,51 +11,80 @@ function FacilityUseData() {
                     <FirestTitle>시설 이용 안내</FirestTitle>
                     <div className="cntTextBox">
                         <div>
-                            <li className="textBoxType1">이용 시간</li>
-                            <table className="">
+                            <li className="textBoxType1 mb-3">이용 시간</li>
+                            <table className="defaultTable">
                                 <thead>
                                     <tr>
-                                        <th>시간</th>
-                                        <th>운영</th>
+                                        <th width="25%">구분</th>
+                                        <th>평일</th>
+                                        <th>토,일요일</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <tr>
+                                        <td>인문사회자연과학실</td>
+                                        <td>09:00 - 20:00</td>
+                                        <td>09:00 - 17:00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>어린이자료실</td>
+                                        <td>09:00 - 20:00</td>
+                                        <td>09:00 - 17:00</td>
+                                    </tr>
+                                    <tr>
+                                        <td>어문학·간행물실</td>
                                         <td>09:00 - 18:00</td>
-                                        <td>운영</td>
+                                        <td>09:00 - 17:00</td>
                                     </tr>
                                     <tr>
-                                        <td>18:00 - 09:00</td>
-                                        <td>점심시간</td>
+                                        <td>디지털자료실</td>
+                                        <td>09:00 - 18:00</td>
+                                        <td>09:00 - 17:00</td>
                                     </tr>
                                     <tr>
-                                        <td>10:00 - 12:00</td>
-                                        <td>간식시간</td>
-                                    </tr>
-                                    <tr>
-                                        <td>12:00 - 13:00</td>
-                                        <td>점심시간</td>
-                                    </tr>
-                                    <tr>
-                                        <td>13:00</td>
+                                        <td>디지털자료실</td>
+                                        <td>
+                                            07:00 - 22:00(하절기 3월 ~ 10월)
+                                            <br />
+                                            08:00 - 22:00(동절기 11월 ~ 2월)
+                                        </td>
+                                        <td>
+                                            07:00 - 22:00(하절기 3월 ~ 10월)
+                                            <br />
+                                            08:00 - 22:00(동절기 11월 ~ 2월)
+                                        </td>
                                     </tr>
                                 </tbody>
                             </table>
                         </div>
                         <div>
-                            <li className="textBoxType1">시설 소개</li>
-                            <li className="imgBox">
-                                <img src={restaurantImg01} alt="식당이미지1" />
-                                <img src={restaurantImg02} alt="식당이미지2" />
-                                <img src={restaurantImg03} alt="식당이미지3" />
-                            </li>
-                        </div>
-                        <div>
-                            <li className="textBoxType1">이용 안내</li>
-                            <li className="textBoxType2">
-                                식당은 따로 운영되지 않으며 개인 도시락 취식 및
-                                멀티자판기 사용가능합니다.
-                            </li>
+                            <li className="textBoxType1 mb-3">휴관일</li>
+                            <table className="defaultTable">
+                                <thead>
+                                    <tr>
+                                        <th width="33%">정기휴관일</th>
+                                        <th width="33%">법정 공휴일</th>
+                                        <th width="33%">임시휴관일</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>매월 첫째, 셋째 금요일</td>
+                                        <td>
+                                            일요일을 제외한 법정 공휴일
+                                            <br />
+                                            (일요일과 겹칠 경우 휴관)
+                                        </td>
+                                        <td>
+                                            도서정리, 보수공사, 장서점검 등의
+                                            <br />
+                                            사유로 휴관이 필요하다고 관장이
+                                            <br />
+                                            지정하는 날
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
--- src/pages/Libabout/datas/FacilityUse/FloorData.js
+++ src/pages/Libabout/datas/FacilityUse/FloorData.js
@@ -1,46 +1,244 @@
-import React from "react";
-import storeImg01 from "../../../../assets/images/storeImg01.png";
-import storeImg02 from "../../../../assets/images/storeImg02.png";
-import storeImg03 from "../../../../assets/images/storeImg03.png";
+import React, { useEffect, useState } from "react";
+import imgFloor1 from "../../../../assets/images/imgFloor1.png";
+import imgFloor2 from "../../../../assets/images/imgFloor2.png";
+import imgFloor3 from "../../../../assets/images/imgFloor3.png";
+import imgFloor4 from "../../../../assets/images/imgFloor4.png";
+import imgFloor5 from "../../../../assets/images/imgFloor5.png";
 import FirestTitle from "../../../../components/TextBoxs/CntTitle";
 import CntWrap from "../../../../components/TextBoxs/CntWrap";
-function FloorData() {
+import Floor4NotebookRoomData from "./FloorDetail/Floor4NotebookRoomData";
+import Floor4SelfStudyData from "./FloorDetail/Floor4SelfStudyData";
+const floorData = {
+    1: {
+        name: "4층",
+        imageUrl: imgFloor1,
+        areas: [
+            {
+                id: 1,
+                name: "자율학습실",
+                coords: "70,56,308,57,308,301,584,299,586,346,616,347,586,381,615,415,585,413,584,689,68,690",
+                shape: "poly",
+                detail: () => <Floor4SelfStudyData />,
+            },
+            {
+                id: 2,
+                name: "노트북전용실",
+                coords: "945,57,1217,58,1215,691,1017,691,1015,433,909,434,908,412,876,410,905,378,879,342,909,342,910,59,919,58,915,37,946,58",
+                shape: "poly",
+                detail: () => <Floor4NotebookRoomData />,
+            },
+            {
+                id: 3,
+                name: "늘배움실",
+                coords: "800,434,827,435,827,462,854,433,1018,434,1018,690,800,690",
+                shape: "poly",
+                detail: () => <Floor4SelfStudyData />,
+            },
+        ],
+    },
+    2: {
+        name: "3층",
+        imageUrl: imgFloor2,
+        areas: [
+            {
+                id: 1,
+                name: "디지털자료실",
+                coords: "908,58,918,58,918,41,948,61,1216,56,1215,690,909,691,908,410,876,410,906,375,878,341,910,341",
+                shape: "poly",
+                detail: () => <Floor4SelfStudyData />,
+            },
+            {
+                id: 2,
+                name: "어문학·간행물실",
+                coords: "69,58,246,58,246,301,465,301,512,264,510,299,584,301,585,344,618,347,587,381,616,416,586,413,586,689,69,690",
+                shape: "poly",
+                detail: () => <Floor4SelfStudyData />,
+            },
+            {
+                id: 3,
+                name: "휴게실",
+                coords: "761,434,787,435,787,459,817,435,839,436,838,688,761,689",
+                shape: "poly",
+                detail: () => <Floor4SelfStudyData />,
+            },
+        ],
+    },
+    3: {
+        name: "2층",
+        imageUrl: imgFloor3,
+        areas: [
+            {
+                id: 1,
+                name: "인문사회자연과학실",
+                coords: "68,58,248,57,249,301,470,300,513,264,513,299,597,304,598,344,628,344,598,379,628,414,599,414,598,692,71,689",
+                shape: "poly",
+                detail: () => <Floor4SelfStudyData />,
+            },
+            {
+                id: 2,
+                name: "강의실2",
+                coords: "926,438,924,415,890,414,920,378,890,346,924,345,925,187,1215,188,1216,436",
+                shape: "poly",
+                detail: () => <Floor4SelfStudyData />,
+            },
+            {
+                id: 3,
+                name: "동아리방",
+                coords: "923,55,948,55,947,40,979,60,1216,59,1215,186,925,187",
+                shape: "poly",
+                detail: () => <Floor4SelfStudyData />,
+            },
+            {
+                id: 4,
+                name: "늘배움실",
+                coords: "648,57,778,56,780,301,733,300,732,263,691,303,648,298",
+                shape: "poly",
+                detail: () => <Floor4SelfStudyData />,
+            },
+        ],
+    },
+    4: {
+        name: "1층",
+        imageUrl: imgFloor4,
+        areas: [
+            {
+                id: 1,
+                name: "어린이자료실",
+                coords: "70,59,416,59,417,141,578,143,580,327,471,329,470,348,432,351,470,388,431,431,469,430,471,676,71,675",
+                shape: "poly",
+                detail: () => <Floor4SelfStudyData />,
+            },
+            {
+                id: 2,
+                name: "강의실1",
+                coords: "961,59,1215,60,1213,329,1047,332,1046,300,1019,331,989,299,990,329,961,333",
+                shape: "poly",
+                detail: () => <Floor4SelfStudyData />,
+            },
+        ],
+    },
+    5: {
+        name: "지하1층",
+        imageUrl: imgFloor5,
+        areas: [
+            {
+                id: 1,
+                name: "시청각실",
+                coords: "816,59,1215,58,1215,503,817,504,817,417,845,415,820,388,845,358,816,356,816,117,844,83,818,83",
+                shape: "poly",
+                detail: () => <Floor4SelfStudyData />,
+            },
+            {
+                id: 2,
+                name: "경로실",
+                coords: "816,505,1216,505,1217,676,818,677,817,627,848,629,816,599,847,565,817,565",
+                shape: "poly",
+                detail: () => <Floor4SelfStudyData />,
+            },
+            {
+                id: 3,
+                name: "강의실3",
+                coords: "418,59,708,60,709,83,681,81,707,109,682,138,710,137,708,201,506,202,504,221,484,202,464,220,463,201,414,203",
+                shape: "poly",
+                detail: () => <Floor4SelfStudyData />,
+            },
+        ],
+    },
+    6: {
+        name: "기타",
+        areas: [
+            {
+                id: 1,
+                name: "단체문고",
+                detail: () => <Floor4SelfStudyData />,
+                // icon: { icon1 },
+            },
+            {
+                id: 2,
+                name: "등나무벤치(정원)",
+                detail: () => <Floor4SelfStudyData />,
+                // icon: { icon2 },
+            },
+            {
+                id: 2,
+                name: "옥상공원(휴게실)",
+                detail: () => <Floor4SelfStudyData />,
+                // icon: { icon3 },
+            },
+        ],
+    },
+};
+const FloorMap = ({ floorInfo, onAreaClick }) => (
+    <div className="floorMap">
+        <img src={floorInfo.imageUrl} useMap="#floorMap" alt={floorInfo.name} />
+        <map name="floorMap">
+            {floorInfo.areas.map((area) => (
+                <area
+                    key={area.id}
+                    shape={area.shape}
+                    coords={area.coords}
+                    alt={area.name}
+                    title={area.name}
+                    onClick={() => onAreaClick(area)}
+                    style={{ cursor: "pointer" }}
+                />
+            ))}
+        </map>
+    </div>
+);
+function FloorData({ onMapClick }) {
+    const [activeFloor, setActiveFloor] = useState("1");
+    const [floorInfo, setFloorInfo] = useState(null);
+    const [activeAreaComponent, setActiveAreaComponent] = useState(null);
+
+    useEffect(() => {
+        setFloorInfo(floorData[activeFloor]);
+    }, [activeFloor]);
+
+    const handleMapClick = (area) => {
+        if (onMapClick) {
+            onMapClick(area);
+        }
+    };
+
     return (
-        <>
-            <CntWrap>
-                {/* Start:큰 단락 복사 사용시 */}
-                <div className="cntWrapBox">
-                    <FirestTitle>매점 안내</FirestTitle>
-                    <div className="cntTextBox">
-                        <div>
-                            <li className="textBoxType2">
-                                <span>무인 매점으로 운영되고 있습니다.</span>
-                            </li>
-                            <li className="textBoxType2">
-                                <span>위치(층) : </span>강서도서관 &#62; 기타
-                                (지하1층 식당에 위치)
-                            </li>
-                        </div>
-                        <div>
-                            <li className="textBoxType1">시설 소개</li>
-                            <li className="imgBox">
-                                <img src={storeImg01} alt="매점 이미지1" />
-                                <img src={storeImg02} alt="매점 이미지2" />
-                                <img src={storeImg03} alt="매점 이미지3" />
-                            </li>
-                        </div>
-                        <div>
-                            <li className="textBoxType1">이용 안내</li>
-                            <li className="textBoxType2">
-                                멀티자판기를 통해 셀프라면, 김밥, 소세지, 과자
-                                등을 판매하고 있습니다.
-                            </li>
+        <CntWrap>
+            <div className="cntWrapBox">
+                <FirestTitle>층별 안내</FirestTitle>
+                <div className="cntTextBox">
+                    <div>
+                        <li className="textBoxType2">
+                            각 실을 클릭하시면 상세 정보를 보실 수 있습니다.
+                        </li>
+                        <div className="floorMapTabs">
+                            <div className="tabs">
+                                {Object.keys(floorData).map((floor) => (
+                                    <button
+                                        key={floor}
+                                        className={`tab ${
+                                            activeFloor === floor
+                                                ? "active"
+                                                : ""
+                                        }`}
+                                        onClick={() => setActiveFloor(floor)}
+                                    >
+                                        {floorData[floor].name}
+                                    </button>
+                                ))}
+                            </div>
+                            <div className="tabContent">
+                                {floorInfo && (
+                                    <FloorMap
+                                        floorInfo={floorInfo}
+                                        onAreaClick={handleMapClick}
+                                    />
+                                )}
+                            </div>
                         </div>
                     </div>
                 </div>
-                {/* End:큰 단락 복사 사용시 */}
-            </CntWrap>
-        </>
+            </div>
+        </CntWrap>
     );
 }
 
