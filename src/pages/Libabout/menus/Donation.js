4b3f8b4118e5a349a872e0803b42af77f95ada40
blueheeya83 <blueheeya12@gmail.com>
2024-09-19 12:20:02 +0900

    수정

--- src/pages/Libabout/menus/Donation.js
+++ src/pages/Libabout/menus/Donation.js
@@ -1,18 +1,18 @@
 import React from "react";
-import MenusScrollFrame from "../../../components/MenusScrollFrame";
 import { SubBackWrap } from "../../../components/Layout/BackWrap";
-import { SubTopMenu } from "../../../components/Layout/TopMenu";
-import DonationData from "../datas/Donation/DonationData";
 import Title from "../../../components/Layout/Title";
+import { SubTopMenu } from "../../../components/Layout/TopMenu";
+import MenusScrollFrame from "../../../components/MenusScrollFrame";
+import DonationData from "../datas/Donation/DonationData";
 function Donation() {
-  const title = "자료기증 관련 안내";
-  return (
-    <SubBackWrap>
-      <SubTopMenu />
-      <Title>{title}</Title>
-      <MenusScrollFrame children={<DonationData />} title={title} />
-    </SubBackWrap>
-  );
+    const title = "자료기증 관련 안내";
+    return (
+        <SubBackWrap>
+            <SubTopMenu />
+            <Title>{title}</Title>
+            <MenusScrollFrame children={<DonationData />} title={title} />
+        </SubBackWrap>
+    );
 }
 
 export default Donation;
