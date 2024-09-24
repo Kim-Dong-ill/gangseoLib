13c5851dae2baef84c677dac4d475a6913f77cc9
Kim-Dong-ill <150985297+Kim-Dong-ill@users.noreply.github.com>
2024-09-25 01:11:23 +0900

    검색

--- src/pages/SerchBook/SerchBook.js
+++ src/pages/SerchBook/SerchBook.js
@@ -4,13 +4,15 @@ import { SubBackWrap } from "../../components/Layout/BackWrap";
 import { DepthTopMenu } from "../../components/Layout/TopMenu";
 import Title from "../../components/Layout/Title";
 import MenusScrollFrame from "../../components/MenusScrollFrame";
+import EbookFrame from "../../components/Ebook/EbookFrame";
 
 function SerchBook() {
   return (
     <SubBackWrap>
       <DepthTopMenu />
       <Title>자료검색</Title>
-      <MenusScrollFrame children={<SerchBookData />} />
+      <EbookFrame children={<SerchBookData />} />
+      {/* <SerchBookData /> */}
     </SubBackWrap>
   );
 }
--- src/pages/SerchBook/data/SerchBookData.js
+++ src/pages/SerchBook/data/SerchBookData.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "../../../assets/css/sass/serchBookData.scss";
-import serchIcon from "../../../assets/icons/tabler_search.svg";
 import InputData from "./InputData";
 
 function SerchBookData() {
   return (
     <div className="serchBookLayout">
       <div className="serchContainer">
-        <InputData />
+        {/* <InputData /> */}
+        <div className="serchIframeWrap">
+          <iframe
+            className="serchIframe"
+            src="https://gslib.sen.go.kr/gslib/intro/search/index.do?menu_idx=4&locExquery=111005&editMode=normal&officeNm=%EA%B0%95%EC%84%9C%EB%8F%84%EC%84%9C%EA%B4%80&lectureNm=&mainSearchType=on&search_text="
+            frameborder="0"
+          ></iframe>
+        </div>
       </div>
     </div>
   );
